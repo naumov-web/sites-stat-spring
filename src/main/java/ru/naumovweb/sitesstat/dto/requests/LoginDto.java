13c0d4e76a898b8f6036214fcf46d0e1e36ea930
Konstantin Naumov <naumov-web@yandex.ru>
2021-02-28 01:24:51 +0300

    Endpoint for login user. Request

---
+++ src/main/java/ru/naumovweb/sitesstat/dto/requests/LoginDto.java
@@ -0,0 +1,17 @@
+package ru.naumovweb.sitesstat.dto.requests;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+public class LoginDto {
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
+    private final String email = null;
+
+    @NotNull(message = "Password cannot be null")
+    private final String password = null;
+}
